1dcb957fcfd47b4d4baef7eb0987b982799c9fb9
majidfelegari <f.majid1377@gmail.com>
2025-11-17 08:19:52 +0330

    Create login page

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -1,9 +1,13 @@
 
-from django.urls import path
+from django.urls import path,include
 
 from . import views
 
 
 app_name = "accounts"
 urlpatterns = [
+    path('login', views.LoginView.as_view(), name="login"),
+    # path('register', views.RegisterView.as_view(), name="register"),
+    # path('login', views.LogoutView.as_view(), name="logout"),
+    # path('',include('django.contrib.auth.urls'))
 ]
--- core/core/urls.py
+++ core/core/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
 
 if settings.DEBUG:
