1ac447d1b16ebb680e46fd38a4ff1196e279b20e
majidfelegari <f.majid1377@gmail.com>
2025-11-17 07:40:27 +0330

    Create an admin profile and admin user

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,73 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Profile
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+User = get_user_model()
+
+class CustomUserAdmin(UserAdmin):
+    """
+    Custom admin panel
+    """
+    model = User
+    list_display = ("id","email","is_superuser", "is_active", "is_verified")
+    list_filter = ("email", "is_superuser", "is_active", "is_verified")
+    search_fields = ("email",)
+    ordering = ("email",)
+    fieldsets = (
+        (
+            "Authentication",
+            {
+                "fields": ("email", "password"),
+            },
+        ),
+        (
+            "permissions",
+            {
+                "fields": (
+                    "is_staff",
+                    "is_active",
+                    "is_superuser",
+                    "is_verified",
+                )
+            }
+        ),
+        (
+            "group permissions",
+            {
+                "fields": ("groups", "user_permissions", "type"),
+            },
+        ),
+        (
+            "important date",
+            {
+                "fields": ("last_login",),
+            }
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                    "is_superuser",
+                    "is_verified",
+                    "type",
+                ),
+            },
+        ),
+    )
+
+class CustomProfileAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "first_name", "last_name", "phone_number")
+    search_fields = ("user", "first_name", "last_name", "phone_number")
+
+
+admin.site.register(Profile, CustomProfileAdmin)
+admin.site.register(User, CustomUserAdmin)
