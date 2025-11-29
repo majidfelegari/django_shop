e4dd19761da490d6aacda36c9f389d13c4d7e123
majidfelegari <f.majid1377@gmail.com>
2025-11-29 13:52:34 +0330

    Add addresses and display order price summary on the order registration page

--- core/order/views.py
+++ core/order/views.py
@@ -1,8 +1,22 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,FormView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from order.permissions import HasCustomerAccessPermission
+from order.models import UserAddressModel
+from order.forms import CheckOutForm
+from cart.models import CartModel
 # Create your views here.
 
 
-class OrderCheckOutView(LoginRequiredMixin, HasCustomerAccessPermission, TemplateView):
+class OrderCheckOutView(LoginRequiredMixin, HasCustomerAccessPermission, FormView):
     template_name = "order/checkout.html"
+    form_class = CheckOutForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cart = CartModel.objects.get(user = self.request.user)
+        context["addresses"] = UserAddressModel.objects.filter(user=self.request.user)
+        total_price = cart.calculate_total_price()
+        context["total_price"] = total_price
+        context["total_tax"] = round((total_price * 9) / 100)
+        return context
+    
