7132f1bd701660609463418619828e1b8a4ceb2a
majidfelegari <f.majid1377@gmail.com>
2025-12-02 16:26:54 +0330

    fix order view

--- core/order/views.py
+++ core/order/views.py
@@ -1,18 +1,25 @@
-from django.views.generic import TemplateView,FormView, View
+from django.http import HttpResponse
+from django.views.generic import (
+    TemplateView,
+    FormView,
+    View
+)
 from django.contrib.auth.mixins import LoginRequiredMixin
 from order.permissions import HasCustomerAccessPermission
-from order.models import UserAddressModel, OrderModel, OrderItemModel, CouponModel
+from order.models import UserAddressModel
 from order.forms import CheckOutForm
-from cart.models import CartModel
+from cart.models import CartModel, CartItemModel
+from order.models import OrderModel, OrderItemModel
 from django.urls import reverse_lazy
 from cart.cart import CartSession
 from decimal import Decimal
-from django.utils import timezone
+from order.models import CouponModel
 from django.http import JsonResponse
-from payment.zarinpal_client import ZarinPalSandbox
+from django.utils import timezone
 from django.shortcuts import redirect
-from payment.models import PaymentModel
 # Create your views here.
+from payment.zarinpal_client import ZarinPalSandbox
+from payment.models import PaymentModel
 
 
 class OrderCheckOutView(LoginRequiredMixin, HasCustomerAccessPermission, FormView):
@@ -26,75 +33,90 @@ class OrderCheckOutView(LoginRequiredMixin, HasCustomerAccessPermission, FormVie
         return kwargs
 
     def form_valid(self, form):
+        user = self.request.user
         cleaned_data = form.cleaned_data
-
         address = cleaned_data['address_id']
         coupon = cleaned_data['coupon']
-        cart = CartModel.objects.get(user = self.request.user)
-        cart_items = cart.cart_items.all()
-        order = OrderModel.objects.create(
-            user = self.request.user,
-            address = address.address,
-            state = address.state,
-            city = address.city,
-            zip_code = address.zip_code,
-        )
-        for item in cart_items:
-            OrderItemModel.objects.create(
-                order = order,
-                product = item.product,
-                quantity = item.quantity,
-                price = item.product.get_price(),
-            )
-        cart_items.delete()
-        CartSession(self.request.session).clear()
+
+        cart = CartModel.objects.get(user=user)
+        order = self.create_order(address)
+
+        self.create_order_items(order, cart)
+        self.clear_cart(cart)
 
         total_price = order.calculate_total_price()
-        if coupon:
-            total_price = total_price - round(total_price * Decimal(coupon.discount_percent/100))
-            order.coupon = coupon
-            coupon.used_by.add(self.request.user)
-            coupon.save()
-        order.total_price = total_price
+        self.apply_coupon(coupon, order, user, total_price)
         order.save()
-
         return redirect(self.create_payment_url(order))
-    
+
     def create_payment_url(self, order):
         zarinpal = ZarinPalSandbox()
-        response = zarinpal.payment_request(order.total_price)
+        response = zarinpal.payment_request(order.get_price())
         payment_obj = PaymentModel.objects.create(
-            authority_id = response.get("Authority"),
-            amount = order.total_price,
+            authority_id=response.get("Authority"),
+            amount=order.get_price(),
         )
         order.payment = payment_obj
         order.save()
         return zarinpal.generate_payment_url(response.get("Authority"))
-        
-    
+
+    def create_order(self, address):
+        return OrderModel.objects.create(
+            user=self.request.user,
+            address=address.address,
+            state=address.state,
+            city=address.city,
+            zip_code=address.zip_code,
+        )
+
+    def create_order_items(self, order, cart):
+        for item in cart.cart_items.all():
+            OrderItemModel.objects.create(
+                order=order,
+                product=item.product,
+                quantity=item.quantity,
+                price=item.product.get_price(),
+            )
+
+    def clear_cart(self, cart):
+        cart.cart_items.all().delete()
+        CartSession(self.request.session).clear()
+
+    def apply_coupon(self, coupon, order, user, total_price):
+        if coupon:
+            # discount_amount = round(
+            #     (total_price * Decimal(coupon.discount_percent / 100)))
+            # total_price -= discount_amount
+
+            order.coupon = coupon
+            coupon.used_by.add(user)
+            coupon.save()
+
+        order.total_price = total_price
+
     def form_invalid(self, form):
         return super().form_invalid(form)
-        
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        cart = CartModel.objects.get(user = self.request.user)
-        context["addresses"] = UserAddressModel.objects.filter(user=self.request.user)
+        cart = CartModel.objects.get(user=self.request.user)
+        context["addresses"] = UserAddressModel.objects.filter(
+            user=self.request.user)
         total_price = cart.calculate_total_price()
         context["total_price"] = total_price
-        context["total_tax"] = round((total_price * 9) / 100)
+        context["total_tax"] = round((total_price * 9)/100)
         return context
-    
 
 
 class OrderCompletedView(LoginRequiredMixin, HasCustomerAccessPermission, TemplateView):
     template_name = "order/completed.html"
-
     
 class OrderFailedView(LoginRequiredMixin, HasCustomerAccessPermission, TemplateView):
     template_name = "order/failed.html"
 
+
 class ValidateCouponView(LoginRequiredMixin, HasCustomerAccessPermission, View):
+
     def post(self, request, *args, **kwargs):
         code = request.POST.get("code")
         user = self.request.user
@@ -105,23 +127,24 @@ class ValidateCouponView(LoginRequiredMixin, HasCustomerAccessPermission, View):
         total_tax = 0
 
         try:
-            coupon = CouponModel.objects.get(code = code)
+            coupon = CouponModel.objects.get(code=code)
         except CouponModel.DoesNotExist:
-            return JsonResponse({"message": "کد تخفیف یافت نشد"}, status = 404)
+            return JsonResponse({"message": "کد تخفیف یافت نشد"}, status=404)
         else:
             if coupon.used_by.count() >= coupon.max_limit_usage:
                 status_code, message = 403, "محدودیت در تعداد استفاده"
 
             elif coupon.expiration_date and coupon.expiration_date < timezone.now():
-                status_code, message = 403, "کد تخفیف منقضی شده"
+                status_code, message = 403, "کد تخفیف منقضی شده است"
 
             elif user in coupon.used_by.all():
-                status_code, message = 403, "این کد تخفیف قبلا توسط شما استفاده شده"
-            
+                status_code, message = 403, "این کد تخفیف قبلا توسط شما استفاده شده است"
+
             else:
-                cart = CartModel.objects.get(user = self.request.user)
+                cart = CartModel.objects.get(user=self.request.user)
 
                 total_price = cart.calculate_total_price()
-                total_price = round(total_price - (total_price * (coupon.discount_percent/100)))
+                total_price = round(
+                    total_price - (total_price * (coupon.discount_percent/100)))
                 total_tax = round((total_price * 9)/100)
-        return JsonResponse({ "message": message,"total_tax":total_tax,"total_price":total_price}, status= status_code)
+        return JsonResponse({"message": message, "total_tax": total_tax, "total_price": total_price}, status=status_code)
